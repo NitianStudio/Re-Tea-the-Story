5e95a2db2ce0657d3268a71c1d9b09fef67353c0
baka4n <qq474899581@gmail.com>
2024-07-08 15:24:43 +0800

    moving package

---
+++ src/neoforge/java/io/github/nitianstudio/reteastory/neoforge/ReteastoryNeoforge.java
@@ -0,0 +1,11 @@
+package io.github.nitianstudio.reteastory.neoforge;
+
+import io.github.nitianstudio.reteastory.Reteastory;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Reteastory.MOD_ID)
+public class ReteastoryNeoforge {
+    public ReteastoryNeoforge() {
+        
+    }
+}
